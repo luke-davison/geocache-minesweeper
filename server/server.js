1a533e8f655a205cdecd8ac3948d9893df8e0049
luke-davison <luke.warmwater6@gmail.com>
2017-07-04 11:30:02 +1200

    changes saved

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const environment = process.env.NODE_ENV || 'development'
-const config = require('./knexfile')[environment]
+const config = require('../knexfile')[environment]
 const knex = require('knex')(config)
 
 const routes = require('./routes')
